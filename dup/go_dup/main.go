ecdd2dcefab4e7036e2fab51f63a218cbd0e8553
sony-nurdianto <sonynurdianto445@gmail.com>
2025-01-09 12:51:49 +0700

    add comment

--- dup/go_dup/main.go
+++ dup/go_dup/main.go
@@ -1,3 +1,5 @@
+// Dup1 prints the text of each line that appears more than
+// once in the standard input, preceded by its count.
 package main
 
 import (
@@ -5,6 +7,14 @@ import (
 	"fmt"
 	"os"
 )
+
+// Program utama untuk menghitung frekuensi kemunculan setiap baris input
+// yang dibaca dari stdin.
+//
+// Program ini akan membaca setiap baris yang dimasukkan, menghitung
+// jumlah kemunculan setiap baris, dan kemudian mencetak hasilnya dalam
+// bentuk `HashMap` di mana kunci adalah baris input dan nilai adalah
+// jumlah kemunculannya.
 
 func main() {
 	counts := make(map[string]int)
--- dup/rust_dup/src/main.rs
+++ dup/rust_dup/src/main.rs
@@ -3,15 +3,24 @@ use std::{
     io::{self, BufRead},
 };
 
+/// Program utama untuk menghitung frekuensi kemunculan setiap baris input
+/// yang dibaca dari stdin.
+///
+/// Program ini akan membaca setiap baris yang dimasukkan, menghitung
+/// jumlah kemunculan setiap baris, dan kemudian mencetak hasilnya dalam
+/// bentuk `HashMap` di mana kunci adalah baris input dan nilai adalah
+/// jumlah kemunculannya.
 fn main() {
+    // Membuat HashMap untuk menyimpan jumlah kemunculan setiap baris.
     let mut counts: HashMap<String, u32> = HashMap::new();
 
-    io::stdin().lock().lines().for_each(|line| match line {
-        Ok(value) => {
+    // Membaca input dari stdin dan memproses setiap barisnya.
+    io::stdin().lock().lines().for_each(|line| {
+        if let Ok(value) = line {
+            // Memperbarui jumlah kemunculan untuk setiap baris
             let count = counts.entry(value).or_insert(0);
             *count += 1;
         }
-        Err(_) => {}
     });
 
     // for line in input.lock().lines() {
@@ -24,5 +33,6 @@ fn main() {
     //     };
     // }
 
+    // Menampilkan hasil penghitungan
     println!("{:?}", counts);
 }

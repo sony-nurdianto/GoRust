067ab426255689af78e7a7357b1cc82454ca9cba
sony-nurdianto <sonynurdianto445@gmail.com>
2025-01-10 15:24:44 +0700

    dup_file no buff

--- dup_file_nobuff/go_dup_file_nobuff/main.go
+++ dup_file_nobuff/go_dup_file_nobuff/main.go
@@ -1,8 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
 
 func main() {
+	counts := make(map[string]int)
+	args := os.Args[1:]
+	fmt.Println("Find Duplicate Word In " + strings.Join(args, " "))
 
-	fmt.Println("Hello, world!")
+	for _, filename := range args {
+		data, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+
+		for _, line := range strings.Split(string(data), "\n") {
+			for _, word := range strings.Split(line, " ") {
+				re := regexp.MustCompile("[^a-zA-Z0-9]")
+				counts[re.ReplaceAllString(strings.TrimSpace(word), "")]++
+			}
+		}
+
+		for k, v := range counts {
+			fmt.Printf("%s: %d\n", k, v)
+		}
+	}
 }
---
+++ dup_file_nobuff/rust_dup_file_nobuff/src/main.rs
@@ -0,0 +1,94 @@
+use std::{
+    collections::HashMap,
+    env,
+    fmt::Formatter,
+    fs,
+    path::Path,
+    sync::{Arc, Mutex},
+};
+use std::{fmt, io::Error as IoError, string::FromUtf8Error};
+
+use lazy_static::lazy_static;
+use rayon::{
+    iter::{IntoParallelRefIterator, ParallelIterator},
+    str::ParallelString,
+};
+use regex::{Error as RgxError, Regex};
+
+lazy_static! {
+    static ref RE: Regex = Regex::new(r"[^a-zA-Z0-9]").unwrap();
+}
+
+#[derive(Debug)]
+enum DupError {
+    Io(IoError),
+    Utf8(FromUtf8Error),
+    Regex(RgxError),
+}
+
+impl fmt::Display for DupError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            DupError::Io(err) => write!(f, "I/O Error: {}", err),
+            DupError::Utf8(err) => write!(f, "UTF-8 Error: {}", err),
+            DupError::Regex(err) => write!(f, "Regex Error: {}", err),
+        }
+    }
+}
+
+impl From<IoError> for DupError {
+    fn from(value: IoError) -> Self {
+        DupError::Io(value)
+    }
+}
+
+impl From<FromUtf8Error> for DupError {
+    fn from(value: FromUtf8Error) -> Self {
+        DupError::Utf8(value)
+    }
+}
+
+impl From<RgxError> for DupError {
+    fn from(value: RgxError) -> Self {
+        DupError::Regex(value)
+    }
+}
+
+fn find_duplicate(path: &Path, counts: &Arc<Mutex<HashMap<String, u32>>>) -> Result<(), DupError> {
+    let fs = fs::read(path)?;
+    let text = String::from_utf8(fs)?;
+    let data = Arc::clone(counts);
+
+    text.par_split_whitespace().for_each(move |word| {
+        let clean_word = RE.replace_all(word, "").to_string();
+        if !clean_word.is_empty() {
+            match data.lock() {
+                Ok(mut value) => *value.entry(clean_word).or_insert(0) += 1,
+                Err(err) => eprintln!("Failed to lock data {}", err),
+            }
+        }
+    });
+
+    Ok(())
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file: &[String] = &args[1..];
+    let counts = Arc::new(Mutex::new(HashMap::<String, u32>::new()));
+
+    println!("Finding duplicate Word In: {}", file.join(" "));
+    file.par_iter().for_each(|arg| {
+        let path: &Path = Path::new(arg);
+
+        match find_duplicate(path, &counts) {
+            Ok(_) => {
+                let count = counts.lock().unwrap();
+                count.par_iter().for_each(|(k, v)| println!("{}:{}", k, v));
+            }
+            Err(err) => {
+                eprintln!("Error Processing File {}: {}", arg, err)
+            }
+        };
+    });
+}

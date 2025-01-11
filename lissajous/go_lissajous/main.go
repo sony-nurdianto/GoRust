f5b31ee075310d5869241c914c01136196f8de47
sony-nurdianto <sonynurdianto445@gmail.com>
2025-01-11 23:59:20 +0700

    finishing exercise

--- lissajous/go_lissajous/main.go
+++ lissajous/go_lissajous/main.go
@@ -13,11 +13,23 @@ import (
 var palette = []color.Color{
 	color.White,
 	color.Black,
+	color.RGBA{R: 255, G: 0, B: 0, A: 255},   // 1 Merah
+	color.RGBA{R: 0, G: 255, B: 0, A: 255},   // 2 Hijau
+	color.RGBA{R: 0, G: 0, B: 255, A: 255},   // 3 Biru
+	color.RGBA{R: 255, G: 255, B: 0, A: 255}, // 4 Kuning
+	color.RGBA{R: 255, G: 0, B: 255, A: 255}, // 5 Ungu
+	color.RGBA{R: 0, G: 255, B: 255, A: 255}, // 6 Cyan
 }
 
 const (
-	whiteIndex = 0 // first color in palette
-	blackIndex = 1 // next color in palette
+	whiteIndex  = 0 // first color in palette
+	blackIndex  = 1
+	redIndex    = 2
+	greenIndex  = 3
+	blueIndex   = 4
+	yellowIndex = 5
+	purpleIndex = 6
+	cyanIndex   = 7
 )
 
 func main() {
@@ -36,11 +48,13 @@ func lissajous(out io.Writer) {
 	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0 // phase difference
+	index_pallete := 1
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
 
 		t := 0.0
+		index_pallete++
 		for t < cycles*2*math.Pi {
 			x := int(math.Sin(t)*float64(size) + 0.5)
 			y := int(math.Sin(t*freq+phase)*float64(size) + 0.5)
@@ -49,11 +63,16 @@ func lissajous(out io.Writer) {
 			pixelY := size + y
 
 			if pixelX >= 0 && pixelY >= 0 && pixelX < 2*size+1 && pixelY < 2*size+1 {
-				img.SetColorIndex(pixelX, pixelY, blackIndex)
+				img.SetColorIndex(pixelX, pixelY, uint8(index_pallete))
 			}
 
 			t += res
 		}
+
+		if index_pallete >= 7 {
+			index_pallete = 0
+		}
+
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
 		anim.Image = append(anim.Image, img)
--- lissajous/rust_lissajous/src/main.rs
+++ lissajous/rust_lissajous/src/main.rs
@@ -16,6 +16,17 @@ const SIZE: u32 = 100;
 const NFRAMES: u32 = 64;
 const DELAY: u32 = 8;
 
+const PALETE: [image::Rgba<u8>; 8] = [
+    image::Rgba([255, 255_u8, 255_u8, 255_u8]), // White
+    image::Rgba([0, 0, 0, 255]),                // Black
+    image::Rgba([255, 0, 0, 255]),              // Red
+    image::Rgba([0, 255, 0, 255]),              // Green
+    image::Rgba([0, 0, 255, 255]),              // Blue
+    image::Rgba([255, 255, 0, 255]),            // Yellow
+    image::Rgba([255, 0, 255, 255]),            // Purple
+    image::Rgba([0, 255, 255, 255]),            // Cyan
+];
+
 fn main() {
     let something = File::create("out.gif").unwrap();
     let freq = rand::thread_rng().gen_range(0.0..3.0);
@@ -24,12 +35,14 @@ fn main() {
 
     let mut phase: f64 = 0.0;
 
-    for _ in 0..NFRAMES {
-        let mut img: RgbaImage = ImageBuffer::from_pixel(
-            2 * SIZE + 1,
-            2 * SIZE * 1,
-            image::Rgba([225, 225, 225, 225]),
-        );
+    let mut index_palete = 1;
+
+    (0..NFRAMES).for_each(|_| {
+        if index_palete >= 7 {
+            index_palete = 0;
+        }
+
+        let mut img: RgbaImage = ImageBuffer::from_pixel(2 * SIZE + 1, 2 * SIZE * 1, PALETE[0]);
 
         let mut t = 0.0;
         while t < CYCLES * 2.0 * PI {
@@ -40,13 +53,14 @@ fn main() {
             let pixel_y = SIZE as i32 + y;
 
             if pixel_x >= 0 && pixel_y >= 0 {
-                img.put_pixel(pixel_x as u32, pixel_y as u32, image::Rgba([0, 0, 0, 225]));
+                img.put_pixel(pixel_x as u32, pixel_y as u32, PALETE[index_palete]);
             }
 
             t += RES;
         }
+        index_palete += 1;
         phase += 0.1;
         let frame = Frame::from_parts(img, 0, 0, Delay::from_numer_denom_ms(DELAY, 1));
         anim.encode_frame(frame).unwrap();
-    }
+    });
 }

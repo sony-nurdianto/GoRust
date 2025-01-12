4cfa7dc005890dab58a3e6f0fe37b466f36c1bce
sony-nurdianto <sonynurdianto445@gmail.com>
2025-01-12 17:48:32 +0700

    fetch

---
+++ fetch/rust_fetch/src/main.rs
@@ -0,0 +1,48 @@
+use core::fmt;
+use std::{
+    env,
+    io::{self, Error, Write},
+};
+
+#[derive(Debug)]
+enum FetchErr {
+    StdError(Error),
+    ReqwestError(reqwest::Error),
+}
+
+impl fmt::Display for FetchErr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::StdError(err) => write!(f, "Std Error: {}", err),
+            Self::ReqwestError(err) => write!(f, "Reqwest Error: {}", err),
+        }
+    }
+}
+
+impl From<io::Error> for FetchErr {
+    fn from(value: io::Error) -> Self {
+        FetchErr::StdError(value)
+    }
+}
+
+impl From<reqwest::Error> for FetchErr {
+    fn from(value: reqwest::Error) -> Self {
+        FetchErr::ReqwestError(value)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), FetchErr> {
+    let args: Vec<String> = env::args().collect();
+
+    for url in &args[1..] {
+        let mut response = reqwest::get(url).await?;
+        let mut out = io::stdout();
+
+        while let Some(chunk) = response.chunk().await? {
+            out.write_all(&chunk)?;
+        }
+    }
+
+    Ok(())
+}

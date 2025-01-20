d71593c09e5fbaa144c75c1dcf0cbe1da18f2cde
sonyDesaTech <sony@desatech.id>
2025-01-20 10:38:17 +0700

    rust_echo4

--- echo4/rust_echo4/src/main.rs
+++ echo4/rust_echo4/src/main.rs
@@ -1,3 +1,35 @@
+use clap::{Arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("example")
+        .arg(
+            Arg::new("n")
+                .short('n')
+                .long("newline")
+                .help("Omit trailing newline")
+                .action(clap::ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("s")
+                .short('s')
+                .long("separator")
+                .help("Separator")
+                .default_value(" "),
+        )
+        .arg(
+            Arg::new("args")
+                .help("Input arguments")
+                .action(clap::ArgAction::Append),
+        )
+        .get_matches();
+
+    let omit_newline = matches.get_flag("n");
+    let separator = matches.get_one::<String>("s").unwrap();
+    let args: Vec<String> = matches.get_many("args").unwrap().cloned().collect();
+
+    if !omit_newline {
+        return println!("{}", args.join(separator));
+    }
+
+    print!("{}", args.join(separator));
 }
